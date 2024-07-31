d1135104214e3ba2bdade1800af1651774c63f2b
zuna21 <adir.zunic@gmail.com>
2024-07-31 13:51:20 +0200

    Added backend

---
+++ Server/API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+}
---
+++ Server/API/Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseNpgsql(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    );
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+// app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
