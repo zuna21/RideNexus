91806228ab36ca6aa184348301c9230aa6cde709
zuna21 <adir.zunic@gmail.com>
2024-08-21 12:13:56 +0200

    Working on notifications

---
+++ Server/API/Data/Migrations/20240820065459_UpdateClientEntity.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateClientEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Latitude",
+                table: "Clients",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Location",
+                table: "Clients",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Longitude",
+                table: "Clients",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "Location",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "Clients");
+        }
+    }
+}
--- Server/API/Entities/Client.cs
+++ Server/API/Entities/Client.cs
@@ -7,6 +7,9 @@ public class Client
     public int Id { get; set; }
     public string Username { get; set; }
     public string Password { get; set; }
+    public double Latitude { get; set; } = 0;
+    public double Longitude { get; set; } = 0;
+    public string Location { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
     public List<Review> Reviews { get; set; } = [];
